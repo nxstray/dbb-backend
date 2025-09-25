18cc6e2a80c363fa128d73e964d25c04db7831a0
nxstray <afwanapriansyah@gmail.com>
2025-09-25 18:15:08 +0700

    start: backend

---
+++ src/main/java/com/dapurBunda/dbb_backend/DbbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dapurBunda.dbb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dapurBunda/dbb_backend/DbbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapurBunda.dbb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
